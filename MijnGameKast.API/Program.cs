f1a5c62d824c915e55d31aa5a38d9210d2ecb690
jeroenWindesheim <s1215501@student.windesheim.nl>
2026-04-11 12:01:30 +0200

    updated the program.cs so I can use modelstate filter myself

--- MijnGameKast.API/Program.cs
+++ MijnGameKast.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace MijnGameKast.API;
 
 public class Program
@@ -10,6 +12,11 @@ public class Program
         builder.Services.AddControllers();
         builder.Services.AddAuthorization();
         builder.Services.AddOpenApi();
+        
+        builder.Services.Configure<ApiBehaviorOptions>(options =>
+        {
+            options.SuppressModelStateInvalidFilter = true;
+        });
 
         var app = builder.Build();
 
@@ -28,84 +35,3 @@ public class Program
         app.Run();
     }
 }
-
-
-// // UserController userController = new UserController();
-//
-//
-// var builder = WebApplication.CreateBuilder(args);
-//
-// // Add services to the container.
-// // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-// builder.Services.AddOpenApi();
-//
-// var app = builder.Build();
-//
-// // Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.MapOpenApi();
-// }
-//
-// app.UseHttpsRedirection();
-//
-// var summaries = new[]
-// {
-//     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-// };
-//
-// var users = new[]
-// {
-//     new { Id = 0, Name = "admin" },
-//     new { Id = 1, Name = "Jeroen" },
-//     new { Id = 2, Name = "Justen" }, 
-//     new { Id = 3, Name = "Jonas" },
-// };
-//
-// var userEndPoint = app.MapGroup("/user");
-//
-// app.MapGet("/weatherforecast", () =>
-//     {
-//         var forecast = Enumerable.Range(1, 5).Select(index =>
-//                 new WeatherForecast
-//                 (
-//                     DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-//                     Random.Shared.Next(-20, 55),
-//                     summaries[Random.Shared.Next(summaries.Length)]
-//                 ))
-//             .ToArray();
-//         return forecast;
-//     })
-//     .WithName("GetWeatherForecast");
-//
-// // app.MapGet("/test/{id}/{test}", (int id, string test) => userController.MyMethod(id, test));
-// // userEndPoint.MapGet("/{id}", (int id) => userController.MyMethod(id, "test"));
-//
-// app.MapGet("/users/{id}", (int id) =>
-// {
-//     // Get user by id
-//     var user = users.FirstOrDefault(u => u.Id == id);
-//     
-//     // Check if user exists and if not return error message
-//     if (user == null)
-//     {
-//         return "No user found!";
-//     }
-//     
-//     // Return the name of the user
-//     return user?.Name;
-// });
-//
-// app.MapGet("/test", () => "get");
-// app.MapPost("/test", () => "post");
-// app.MapPut("/test", () => "put");
-// app.MapPatch("/test", () => "patch");
-// app.MapDelete("/test", () => "delete");
-//
-//
-// app.Run();
-//
-// record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-// {
-//     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-// }
