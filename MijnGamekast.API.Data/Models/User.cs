05d1621108cd67a6503b3b819ea56f61eb93f28a
jeroenWindesheim <s1215501@student.windesheim.nl>
2026-04-16 14:28:35 +0200

    Changed the game and user model

--- MijnGamekast.API.Data/Models/User.cs
+++ MijnGamekast.API.Data/Models/User.cs
@@ -5,5 +5,18 @@ namespace MijnGameKast.API.Data.Models;
 public class User
 {
     public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
+    
+    [Display(Name = "Gebruikersnaam")]
+    [Required(ErrorMessage = "Vul uw {0} in.")]
+    public string Username { get; set; }  = string.Empty;
+    
+    [Display(Name = "E-mailadres")]
+    [Required(ErrorMessage = "Vul uw {0} in.")]
+    [EmailAddress(ErrorMessage = "Vul een geldig e-mailadres in.")]
+    public string Email { get; set; }  = string.Empty;
+    
+    [Required]
+    public string PasswordHash { get; set; }  = string.Empty;
+    
+    public DateTime CreatedAt { get; set; }
 }
