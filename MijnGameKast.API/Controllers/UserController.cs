ae8496f74e7773f866c8f1f056119721f1f2104c
jeroenWindesheim <s1215501@student.windesheim.nl>
2026-04-16 14:33:49 +0200

    Commented out unused methods and static user data in `UserController`.

--- MijnGameKast.API/Controllers/UserController.cs
+++ MijnGameKast.API/Controllers/UserController.cs
@@ -9,64 +9,64 @@ namespace MijnGameKast.API.Controllers;
 [Route("api/[controller]")]
 public class UserController : ControllerBase
 {
-    private static List<User> _users = new List<User>()
-    {
-        new User() { Id = 1, Name = "Jeroen" },
-        new User() { Id = 2, Name = "John" },
-        new User() { Id = 3, Name = "Maarten" }
-    };
-
-    [HttpGet]
-    public IActionResult Get()
-    {
-        return Ok(_users);
-    }
-
-    [HttpGet("{id}")]
-    public IActionResult GetById(int id)
-    {
-        User? user = _users.FirstOrDefault(u => u.Id == id);
-        if (user == null)
-        {
-            return NoContent();
-        }
-        
-        return Ok(user);
-    }
-
-    [HttpPost]
-    public IActionResult Post([FromBody] User user)
-    {
-        Console.WriteLine("Console test");
-        Console.WriteLine(user);
-        User newUser = new User() { Id = user.Id, Name = user.Name };
-        _users.Add(newUser);
-        return CreatedAtAction(
-            nameof(GetById),
-            new { id = newUser.Id },
-            newUser
-        );
-    }
-
-    [HttpDelete]
-    public IActionResult Delete(int? id)
-    {
-        User? user = _users.FirstOrDefault(u => u.Id == id);
-
-        if (user == null)
-        {
-            return NoContent();
-        }
-
-        _users.Remove(user);
-        
-        return Ok($"User with id {id} successfully deleted");
-    }
-
-    [HttpPut]
-    public IActionResult Update(int? id)
-    {
-        Console.WriteLine("Er is een user geupdate");
-        return Ok("User successfully updated");
-    }
+    // private static List<User> _users = new List<User>()
+    // {
+    //     new User() { Id = 1, Name = "Jeroen" },
+    //     new User() { Id = 2, Name = "John" },
+    //     new User() { Id = 3, Name = "Maarten" }
+    // };
+    //
+    // [HttpGet]
+    // public IActionResult Get()
+    // {
+    //     return Ok(_users);
+    // }
+    //
+    // [HttpGet("{id}")]
+    // public IActionResult GetById(int id)
+    // {
+    //     User? user = _users.FirstOrDefault(u => u.Id == id);
+    //     if (user == null)
+    //     {
+    //         return NoContent();
+    //     }
+    //     
+    //     return Ok(user);
+    // }
+    //
+    // [HttpPost]
+    // public IActionResult Post([FromBody] User user)
+    // {
+    //     Console.WriteLine("Console test");
+    //     Console.WriteLine(user);
+    //     User newUser = new User() { Id = user.Id, Name = user.Name };
+    //     _users.Add(newUser);
+    //     return CreatedAtAction(
+    //         nameof(GetById),
+    //         new { id = newUser.Id },
+    //         newUser
+    //     );
+    // }
+    //
+    // [HttpDelete]
+    // public IActionResult Delete(int? id)
+    // {
+    //     User? user = _users.FirstOrDefault(u => u.Id == id);
+    //
+    //     if (user == null)
+    //     {
+    //         return NoContent();
+    //     }
+    //
+    //     _users.Remove(user);
+    //     
+    //     return Ok($"User with id {id} successfully deleted");
+    // }
+    //
+    // [HttpPut]
+    // public IActionResult Update(int? id)
+    // {
+    //     Console.WriteLine("Er is een user geupdate");
+    //     return Ok("User successfully updated");
+    // }
 }
