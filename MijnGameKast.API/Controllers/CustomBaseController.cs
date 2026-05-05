c9b29285cfded32463fb2de9f5b40bf3a5c214b0
jeroenWindesheim <s1215501@student.windesheim.nl>
2026-05-05 20:49:18 +0200

    Added `CustomBaseController` with utility methods for standardized action result handling and user/session data retrieval

---
+++ MijnGameKast.API/Controllers/CustomBaseController.cs
@@ -0,0 +1,52 @@
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using MijnGameKast.API.Data.Models;
+using MijnGameKast.API.Services.Results;
+
+namespace MijnGameKast.API.Controllers;
+
+public class CustomBaseController : ControllerBase
+{
+    protected IActionResult ToActionResult(ServiceResult result)
+    {
+        return result.Type switch
+        {
+            ServiceResultType.Success => Ok(result),
+            ServiceResultType.NotFound => NotFound(result),
+            ServiceResultType.Unauthorized => Unauthorized(result),
+            ServiceResultType.Forbidden => StatusCode(403, result),
+            ServiceResultType.BadRequest => BadRequest(result),
+            _ => StatusCode(500, result)
+        };
+    }
+
+    protected Dictionary<string, List<string>> GetValidationErrors()
+    {
+        return ModelState
+            .Where(x => x.Value is not null && x.Value.Errors.Count > 0)
+            .ToDictionary(
+                x => x.Key,
+                x => x.Value!.Errors.Select(e => e.ErrorMessage).ToList()
+            );
+    }
+
+    protected string? GetToken()
+    {
+        return HttpContext.Items["Token"] as string;
+    }
+
+    protected int? GetUserId()
+    {
+        return HttpContext.Items["UserId"] as int?;
+    }
+
+    protected User? GetUser()
+    {
+        return HttpContext.Items["User"] as User;
+    }
+
+    protected Session? GetSession()
+    {
+        return HttpContext.Items["Session"] as Session;
+    }
+}
