7f45629012b8d48f8c2e9569d6579568e7311436
jeroenWindesheim <s1215501@student.windesheim.nl>
2026-05-05 20:50:14 +0200

    Added `Role` property to `AuthResult` model to support user role inclusion

--- MijnGamekast.API.Data/Models/Auth/AuthResult.cs
+++ MijnGamekast.API.Data/Models/Auth/AuthResult.cs
@@ -9,4 +9,5 @@ public class AuthResult
     public int? UserId { get; set; }
     public string? Username { get; set; }
     public string? Email { get; set; }
+    public UserRole? Role { get; set; }
 }
