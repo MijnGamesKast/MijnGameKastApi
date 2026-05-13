85e64f39f92483039f36ac277298ede8f8867b38
jeroenWindesheim <s1215501@student.windesheim.nl>
2026-05-13 22:19:29 +0200

    Added the service for genres

---
+++ MijnGameKast.API/Services/GenreService.cs
@@ -0,0 +1,42 @@
+using MijnGameKast.API.Data.Interfaces;
+using MijnGameKast.API.Data.Models;
+using MijnGameKast.API.Services.Interfaces;
+using MijnGameKast.API.Services.Results;
+
+namespace MijnGameKast.API.Services;
+
+public class GenreService : IGenreService
+{
+    private readonly IGenreRepository _genreRepository;
+
+    public GenreService(IGenreRepository genreRepository)
+    {
+        _genreRepository = genreRepository;
+    }
+    
+    public async Task<List<Genre>> GetAllAsync()
+    {
+        return await _genreRepository.GetAllAsync();
+    }
+
+    public async Task<Genre?> GetByIdAsync(int id)
+    {
+        return await _genreRepository.GetByIdAsync(id);
+    }
+
+    public async Task<Genre> AddGenre(Genre genre)
+    {
+        return await _genreRepository.AddGenreAsync(genre);
+    }
+
+    public async Task<ServiceResult> DeleteGenreAsync(int id)
+    {
+        var result = await _genreRepository.DeleteGenreAsync(id);
+
+        return result switch
+        {
+            true => new ServiceResult { Success = true, Message = $"Genre ({id}) is successvol verwijderd", Type = ServiceResultType.Success },
+            false => new ServiceResult { Message = "Genre kan niet worden gevonden", Type = ServiceResultType.NotFound }
+        };
+    }
+}
---
+++ MijnGameKast.API/Services/interfaces/IGenreService.cs
@@ -0,0 +1,12 @@
+using MijnGameKast.API.Data.Models;
+using MijnGameKast.API.Services.Results;
+
+namespace MijnGameKast.API.Services.Interfaces;
+
+public interface IGenreService
+{
+    Task<List<Genre>> GetAllAsync();
+    Task<Genre?> GetByIdAsync(int id);
+    Task<Genre> AddGenre(Genre genre);
+    Task<ServiceResult> DeleteGenreAsync(int id);
+}
