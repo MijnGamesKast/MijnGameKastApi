bcc588927ca2dc9f61ccec4fdd2e43b0ba942edd
jeroenWindesheim <s1215501@student.windesheim.nl>
2026-05-13 23:27:30 +0200

    Added the repository and interface for managing game genres

---
+++ MijnGamekast.API.Data/Interfaces/IGameGenreRepository.cs
@@ -0,0 +1,14 @@
+using MijnGameKast.API.Data.Models;
+
+namespace MijnGameKast.API.Data.Interfaces;
+
+public interface IGameGenreRepository
+{
+    Task<List<GameGenre>> GetByGameIdAsync(int gameId);
+    Task<List<GameGenre>> GetByGenreIdAsync(int genreId);
+    Task<GameGenre?> GetByIdsAsync(int gameId, int genreId);
+    Task<GameGenre> AddAsync(GameGenre gameGenre);
+    Task<List<GameGenre>> AddMultipleAsync(List<GameGenre> gameGenres);
+    Task<bool> DeleteAsync(int gameId, int genreId);
+    Task<int> DeleteByGameIdAsync(int gameId);
+}
