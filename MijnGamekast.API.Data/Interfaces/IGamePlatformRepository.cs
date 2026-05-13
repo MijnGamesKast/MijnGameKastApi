dafdd614b5cedd6f2888d9e015eade5afdb374c7
jeroenWindesheim <s1215501@student.windesheim.nl>
2026-05-13 22:21:12 +0200

    Added the `interface` for `IGamePlatformRepository`

---
+++ MijnGamekast.API.Data/Interfaces/IGamePlatformRepository.cs
@@ -0,0 +1,10 @@
+namespace MijnGameKast.API.Data.Interfaces;
+
+public interface IGamePlatformRepository
+{
+    // Get All Games by Platform (example all PlayStation games)
+    // Get All Platform By Game (All platform where GTA V is on)
+    
+    // Add a Game with one Platform
+    // Add a Game with multiple Platforms
+}
