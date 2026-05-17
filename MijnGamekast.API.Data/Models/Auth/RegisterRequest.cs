d5f6e0beb13a9bddda4c7dc85d7066d78db9ab65
jeroenWindesheim <s1215501@student.windesheim.nl>
2026-05-17 22:23:22 +0200

    Added max length validation for `Username` and `Email` fields in `RegisterRequest`, updated error messages for consistency.

--- MijnGamekast.API.Data/Models/Auth/RegisterRequest.cs
+++ MijnGamekast.API.Data/Models/Auth/RegisterRequest.cs
@@ -5,14 +5,16 @@ namespace MijnGameKast.API.Data.Models.Auth;
 public class RegisterRequest
 {
     [Required(ErrorMessage = "Vul een gebruikersnaam in.")]
+    [MaxLength(30, ErrorMessage = "Het e-mailadres mag maximaal {1} tekens lang zijn.")]
     public string Username { get; set; } = string.Empty;
     
     [Required(ErrorMessage = "Vul een e-mailadres in.")]
     [EmailAddress(ErrorMessage = "Vul een geldig e-mailadres in.")]
+    [MaxLength(254, ErrorMessage = "Het e-mailadres mag maximaal {1} tekens lang zijn.")]
     public string Email { get; set; } = string.Empty;
     
     [Required(ErrorMessage = "Vul een wachtwoord in.")]
-    [MinLength(8, ErrorMessage = "Het wachtwoord moet minimaal 8 tekens lang zijn.")]
+    [MinLength(8, ErrorMessage = "Het wachtwoord moet minimaal {1} tekens lang zijn.")]
     [RegularExpression(
         @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).{8,}$",
         ErrorMessage = "Het wachtwoord moet minimaal één kleine letter, één hoofdletter, één cijfer en één speciaal teken bevatten."
